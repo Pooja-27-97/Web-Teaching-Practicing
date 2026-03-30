b6416b63152b4e499f44b0d77b837dfdfa8e4d17
Pooja-27 <poojarm2797@gmail.com>
2026-03-30 23:23:49 +0530

    Practice question

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-if(NaN) {
-    console.log("Truthy");
+let num1 = parseInt(prompt("Enter an integer"));
+// let num2 = parseInt(prompt("Enter an integer"));
+let og = num1;
+let rev = 0;
+while(num1 > 0) {
+    let lastDigit = num1 % 10;
+    rev = (rev * 10) + lastDigit;
+    num1 = Math.floor(num1 / 10);
+}
+
+if(og == rev) {
+    console.log("Palindrome")
 } else {
-    console.log("Falsy");
+    console.log("Not a palindrome");
 }
